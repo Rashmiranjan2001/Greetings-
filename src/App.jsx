f28b19095d4307fafeb41638fe3516181de0ae91
Rashmiranjan Rout <89479171+Rashmiranjan2001@users.noreply.github.com>
2021-12-23 23:47:27 +0530

    Uploaded
    I have made a greetings app where it will show Good Morning, Good Afternoon and Good Night according to the times and colour will also change according to the time

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function App() {
+    let curDate = new Date();
+    curDate = curDate.getHours();
+
+    let greeting = "";2
+    const cssStyle = {};
+
+    if (curDate >= 1 && curDate < 12) {
+        greeting = "Good morning";
+        cssStyle.color = "green";
+    } else if (curDate >= 12 && curDate < 19) {
+        greeting = "Good afternoon";
+        cssStyle.color = "blue";
+    } else {
+        greeting = "Good Night";
+        cssStyle.color = "black";
+    }
+
+    return (
+        <React.Fragment>
+            <div>
+                <h1>
+                    Hello Rashmiranjan, <span style={cssStyle}>{greeting}  </span> 
+                </h1>,
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default App;
